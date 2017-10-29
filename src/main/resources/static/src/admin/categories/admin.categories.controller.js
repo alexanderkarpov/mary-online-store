957a13bb114b15f70fc65642b87a1ed673af0f96
Alexander Karpov <alexander.karpoff@gmail.com>
2017-10-29 19:24:21 +0200

    admin page

--- src/main/resources/static/src/admin/categories/admin.categories.controller.js
+++ src/main/resources/static/src/admin/categories/admin.categories.controller.js
@@ -4,14 +4,23 @@
     angular.module('admin')
         .controller('AdminCategoriesController', AdminCategoriesController);
 
-    AdminCategoriesController.$inject = ['AdminCategoriesService', 'categories'];
+    AdminCategoriesController.$inject = ['AdminCategoriesService', 'categories', '$state'];
 
-    function AdminCategoriesController(AdminCategoriesService, categories) {
+    function AdminCategoriesController(AdminCategoriesService, categories, $state) {
         var controller = this;
         controller.items = categories;
 
         controller.remove = function (id) {
             console.log("remove!!!", id);
+            AdminCategoriesService.delete(id)
+                .then(function (response) {
+                    console.log("successfully deleted", response);
+                    $state.reload();
+                })
+                .catch(function (error) {
+                    console.error("something went terribly wrong", error);
+                    $state.reload();
+                });
         };
     }
 
